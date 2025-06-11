3ba7d64f993b555445d14a5ee9588d3665927dc4
harman8055v <harmanbatish11@gmail.com>
2025-06-12 00:36:06 +0530

    Delete components/ProfileProgressBar.tsx

--- components/ProfileProgressBar.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { Progress } from '@/components/ui/progress';
-
-export default function ProfileProgressBar({ progress }: { progress: number }) {
-  return <Progress value={progress} />;
-}
